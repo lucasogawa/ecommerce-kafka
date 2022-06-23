43c99e70d6a6bee28ead60e9b26ac5c140ff7b54
Lucas Akio Ogawa <ogawalucas@gmail.com>
2022-06-23 17:06:22 -0300

    Adding Consumer

---
+++ src/main/java/com/ogawalucas/ecommercekafka/FraudDetectorService.java
@@ -0,0 +1,58 @@
+package com.ogawalucas.ecommercekafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+@Slf4j
+public class FraudDetectorService {
+
+    public static void main(String[] args) {
+        var consumer = new KafkaConsumer<String, String>(properties());
+
+        consumer.subscribe(List.of("ECOMMERCE_NEW_ORDER"));
+
+        while (true) {
+            var records = consumer.poll(Duration.ofMillis(100));
+
+            if (!records.isEmpty()) {
+                log.info("Found" + records.count() + "records!");
+
+                for(var record : records) {
+                    log.info("====================================");
+                    log.info("Processing new order, checking for fraud...");
+                    log.info("KEY:       " + record.key());
+                    log.info("VALUE:     " + record.value());
+                    log.info("PARTITION: " + record.partition());
+                    log.info("OFFSET:    " + record.offset());
+
+                    sleep(5000);
+                }
+            }
+        }
+    }
+
+    public static Properties properties() {
+        var properties = new Properties();
+
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+
+        return properties;
+    }
+
+    private static void sleep(long time) {
+        try {
+            Thread.sleep(time);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
